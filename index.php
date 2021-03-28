86417f0e5f82aa99df47a1c054921de5098d88be
Dmitriy Gulyaev <80756547+Moxckerry@users.noreply.github.com>
2021-03-28 20:43:18 +0400

    Update index.php

--- index.php
+++ index.php
@@ -1,46 +1,29 @@
+<?php session_start(); ?>
 <?php require_once "form.php" ?>
-
 <!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>Домашнее задание 3</title>
-        <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="main.css">
 </head>
 <body>
-<div class="reg-form">
-
     <form action="./" method="post">
-        <label>Имя</label><br>
-            <input type="text" name="firstname" placeholder="Введите имя"><br>
-        <label>Фамилия</label><br>
-            <input type="text" name="lastname" placeholder="Введите фамилию"><br>
-        <label>Логин</label><br>
-            <input type="text" name="login" placeholder="Введите логин"><br>
-        <label>Пароль</label><br>
-            <input type="password" name="password" placeholder="Введите пароль"><br>
-        <button>Отправить</button><br>
+        <label>Имя</label>
+        <input type="text" name="firstname" placeholder="Введите имя">
+        <label>Фамилия</label>
+        <input type="text" name="lastname" placeholder="Введите фамилию">
+        <label>Логин</label>
+        <input type="text" name="login" placeholder="Введите логин">
+        <label>Пароль</label>
+        <input type="password" name="password" placeholder="Введите пароль">
+        <button>Отправить</button>
+        <?php
+        if ($_SESSION['message']) {
+            echo '<p class="msg"> ' .$_SESSION['message'] . ' </p>';
+        }
+        unset($_SESSION['message']);
+        ?>
     </form>
-
-    <?php $validate = valid($_POST) ?>
-    <?php if(!empty($validate['error']) && $validate['error']): ?>
-    <?php foreach ($validate['messages'] as $message): ?>
-            <p style="...">
-            <?= $message ?>
-            </p>
-    <?php endforeach; ?>
-    <?php endif; ?>
-    <?php if(!empty($validate['success']) && $validate['success']): ?>
-    <?php foreach ($validate['messages'] as $message): ?>
-            <p style="...">
-            <?= $message ?>
-            </p>
-    <?php endforeach; ?>
-    <?php endif; ?>
-
-</div>
 </body>
 </html>
-
-
-
