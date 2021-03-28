31c944271c9af9ddfedcb389e44314d898b1c45f
Dmitriy Gulyaev <80756547+Moxckerry@users.noreply.github.com>
2021-03-28 20:44:53 +0400

    Update form.php

--- form.php
+++ form.php
@@ -1,104 +1,46 @@
 <?php
-
-function valid(array $post): array {
-    $validate = [
-        'error' => false,
-        'success' => false,
-        'messages' => [],
-    ];
-
-    if(!empty($post['firstname']) && !empty($post['lastname']) && !empty($post['login']) && !empty($post['password'])){
-
-        $firstname = trim($post['firstname']);
-        $lastname = trim($post['lastname']);
-        $login = trim($post['login']);
-        $password = trim($post['password']);
-
-        $constrains = [
-            'login' => 3,
-            'password' => 4
-        ];
-
-        $validateForm = validLogin($firstname, $lastname, $login, $password, $constrains);
-
-        if (!$validateForm['firstname']) {
-            $validate['error'] = true;
-            array_push(
-                $validate['messages'],
-                "Имя не должно содержать цифр"
-            );
-        }
-
-        if (!$validateForm['lastname']) {
-            $validate['error'] = true;
-            array_push(
-                $validate['messages'],
-                "Фамилия не должна содержать цифр"
-            );
-        }
-
-        if (!$validateForm['login']) {
-            $validate['error'] = true;
-            array_push(
-                $validate['messages'],
-                "Логин должен быть больше {$constrains['login']} знаков"
-            );
-        }
-
-        if (!$validateForm['password']) {
-            $validate['error'] = true;
-            array_push(
-                $validate['messages'],
-                "Пароль должен быть больше {$constrains['password']} знаков"
-            );
-        }
-
-        if (!$validateForm['error']) {
-            $validate['success'] = true;
-            array_push(
-                $validate['messages'],
-                "Вы успешно прошли валидацию",
-                        "Ваше имя : $firstname",
-                        "Ваша фамилия : $lastname",
-                        "Ваш логин : $login",
-                        "Ваш пароль : $password"
-            );
-        }
-
-        return $validate;
-
+    session_start();
+    $firstname = $_POST['firstname'];
+    $lastname = $_POST['lastname'];
+    $login = $_POST['login'];
+    $password = $_POST['password'];
+    $array = [];
+    if(!preg_match("/^([0-9])+$/", $firstname)) {
+        $a = 1;
+    } else {
+        $a = [];
+        $array[] = 'Имя не должно содержать цифр';
     }
-
-    return $validate;
-
-}
-
-
-function validLogin(string $firstname, string $lastname, string $login, string $password, array $constrains): array {
-
-    $validateForm = [
-        'firstname' => true,
-        'lastname' => true,
-        'login' => true,
-        'password' => true,
-    ];
-
-    if (preg_match('/^[\d]+$/', $firstname)) {
-        $validateForm['firstname'] = false;
+    if(!preg_match("/^([0-9])+$/", $lastname)) {
+        $b = 1;
+    } else {
+        $b = [];
+        $array[] = 'Фамилия не должна содержать цифр';
     }
-
-    if (preg_match('/^[\d]+$/', $lastname)) {
-        $validateForm['lastname'] = false;
+    if(strlen($login) > 3) {
+        $c = 1;
+    } else {
+        $c = [];
+        $array[] = 'Логин должен быть больше 3 символов';
     }
-
-    if(strlen($login) < $constrains['login']) {
-        $validateForm['login'] = false;
+    if(strlen($password) > 4) {
+        $d = 1;
+    } else {
+        $d = [];
+        $array[] = 'Пароль должен быть больше 4 символов';
     }
-
-    if(strlen($password) < $constrains['password']) {
-        $validateForm['password'] = false;
+    if (empty($firstname) || empty($lastname) || empty($login) || empty($password)) {
+        $array = [];
     }
-
-    return $validateForm;
-
-}
+    if (!empty($a) && !empty($b) && !empty($c) && !empty($d)) {
+        $_SESSION['message'] = "
+                        Вы успешно прошли валидацию<br> 
+                        Ваше имя: $firstname<br> 
+                        Ваша фамилия: $lastname<br>
+                        Ваш логин: $login<br>
+                        Ваш пароль: $password";
+    } else {
+        $_SESSION['message'] = implode('<br>', $array);
+    }
+    $array = [];
+    $_POST = [];
